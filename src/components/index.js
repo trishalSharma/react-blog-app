85125bce7b0f5716b526fe58ffe9ddd018a6cf32
trishalSharma <trishalsharma01@gmail.com>
2026-01-22 22:53:32 +0530

    export container via full path

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container/Container";
+import Container from "../components/Container/Container.jsx";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
