7ad911de4ef0805cf582197b402c4f3e9483508f
trishalSharma <trishalsharma01@gmail.com>
2026-01-22 22:49:32 +0530

    Rename containe folder to sentence case

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container.jsx";
+import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
