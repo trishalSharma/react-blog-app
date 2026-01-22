156ef93d68b315fe478dd59c19b6692fd9a0bba7
trishalSharma <trishalsharma01@gmail.com>
2026-01-22 22:10:06 +0530

    Fix case-sensitive Container export path

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
+import Container from "./container/Container.jsx";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
@@ -12,8 +12,6 @@ import AuthLayout from "./AuthLayout";
 import Select from "./Select";
 import Button from "./Button";
 import Input from "./Input";
-
-
 
 
 export{
