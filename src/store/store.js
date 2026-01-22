274579c3a344d50cd09f7ba890a7102c408f3ea3
trishalSharma <trishalsharma01@gmail.com>
2026-01-22 21:26:02 +0530

    Fix case-sensitive import for authSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './authSlice'
+import authSlice from './authslice'
 
 // todo: adding listeners - saving theme, saving login session, saving user's preference.
 
